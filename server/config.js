5fbbdb8dc69f4301f686a10988e232a86a432fdc
Ania Kujawa <kujawanna@gmail.com>
2019-06-01 23:40:04 +0200

    first step to depoy

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    username: process.env.User,
-    password: process.env.UserPassword,
-};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const user = require('./routes/User');
-// const config = require('./config');
+require('dotenv').config();
 
 const app = express();
 
